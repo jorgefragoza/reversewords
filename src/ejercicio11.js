9c2817b34725b51f0902092ab953d4c8b43fed27
Jorge Fragoza <jorge.fragoza@coppel.com>
2018-05-30 14:21:04 -0700

    se agregan comentarios con el funcionamiento en general

--- src/ejercicio11.js
+++ src/ejercicio11.js
@@ -1,3 +1,8 @@
+/**
+ * Given an input string, reverse the string word by word.
+ * For example, given s = "the sky is blue", return "blue is sky the". 
+ */
+
 class Ejercicio11 {
     constructor() {
         this.reverse = function(words) {
--- src/ejercicio13.js
+++ src/ejercicio13.js
@@ -1,3 +1,16 @@
+/**
+ * Given two words, beginWord and endWord, and a wordList of approved words,
+ * find the length of the shortest transformation sequence from beginWord to endWord such that:
+ * Only one letter can be changed at a time
+ * Each transformed word must exist in the wordList.
+ * Return the length of the shortest transformation sequence,
+ * or 0 if no such transformation sequence exists.
+ * Note: beginWord does not count as a transformed word.
+ * You can assume that beginWord and endWord are not empty and are not the same.
+ * Example:  For beginWord = "hit", endWord = "cog", and wordList = ["hot", "dot", "lot", "log", "cog"],
+ * the output should be wordLadder(beginWord, endWord, wordList) = 5.
+ * The shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog" with a length of 5
+ */
 class Ejercicio13 {
     constructor() {
         const verifyInput = function(beginWord, endWord, listWord) {
